da619afb6d2e2e806ca787042f7d0b145274d323
GaelErhlich <idiotdu92@gmail.com>
2021-05-05 11:04:56 +0200

    Classes Arbre et ArbreType

--- src/et3/java/projet/entities/trees/Arbre.java
+++ src/et3/java/projet/entities/trees/Arbre.java
@@ -1,7 +1,17 @@
 package et3.java.projet.entities.trees;
 
+import java.util.Date;
+
 public class Arbre {
-    private String genre;
+
+    private ArbreType type;
+    private String adresse;
+    private int circonference;
+    private float hauteur;
+    private boolean estAdulte;
+    private float coordonnees[]; // 2 coordonnnées GPS
+    private boolean estRemarquable;
+    private Date remarquableDate;
 
 
 }
--- src/et3/java/projet/entities/trees/ArbreType.java
+++ src/et3/java/projet/entities/trees/ArbreType.java
@@ -1,2 +1,7 @@
-package et3.java.projet.entities.trees;public class ArbreType {
+package et3.java.projet.entities.trees;
+
+public class ArbreType {
+    private String genre;
+    private String espece;
+    private String nomCommun;
 }
