854a96bc6442baa07dff686734a81d303ab0525a
GaelErhlich <idiotdu92@gmail.com>
2021-05-28 15:43:52 +0200

    Edited Main

--- src/et3/java/projet/application/Main.java
+++ src/et3/java/projet/application/Main.java
@@ -1,37 +1,81 @@
 package et3.java.projet.application;
 
 import java.io.File;
+import java.util.Scanner;
 
 import et3.java.projet.data.FileReader;
 
 public class Main 
 {
-	/*TEST*/
+
 	public static void main(String[] args) 
-	{		
-		if(args.length > 0)
-		{
-			File tempFile = new File(args[0]);
-			
-			if(tempFile.exists())
-			{
-				System.out.println("[Main] Reading the file " + args[0] + " ...");
-						
-				//We start by reading the CSV file
-				FileReader.getDataFromCSVFile(args[0]);
-				
-				System.out.println("[Main] End of the file " + args[0] + ".");
+	{
+		Scanner scanner = new Scanner(System.in);
+		boolean doitEteindre = false;
+
+
+		while(!doitEteindre) {
+
+			System.out.println(
+				"Tapez :"
+					+"\n> quitter : Fermer l'application"
+					+"\n"
+			);
+			String commande = scanner.nextLine();
+
+			switch (commande) {
+
+
+
+
+
+
+				case "membre":
+					System.out.println(
+							"Tapez :"
+							+"\n> "
+							+"\n"
+					);
+
+
+
+
+
+
+				case "quitter":
+					System.out.println("Extinction du programme...");
+					doitEteindre = true;
+					break;
+
+
+				default:
+					System.out.println("La commande '"+commande+"' n'est pas reconnue.");
+
 			}
-			else
-			{
-				System.out.println("[Main] No file " + args[0]);
-			}
-		}
-		else
-		{
-			System.out.println("[Main] You should enter the CSV file path as a parameter.");
+
 		}
 		
 		//TODO Project :)
+	}
+
+
+	private void chargeDatabase() {
+		String path = "src\\et3\\java\\projet\\data\\data.csv";
+
+		File tempFile = new File(path);
+
+		if(tempFile.exists())
+		{
+			System.out.println("[Main] Reading the file " + path + " ...");
+
+			//We start by reading the CSV file
+			FileReader.getDataFromCSVFile(path);
+
+			System.out.println("[Main] End of the file " + path + ".");
+		}
+		else
+		{
+			System.out.println("[Main] No file " + path);
+		}
 	}
 }
